7cabe9791e9752359821b017f13a8699fe26346a
Kirill Kontorovich <ajiykapd100@gmail.com>
2020-10-19 15:48:07 +0300

    change publish settings

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,9 @@ module.exports = {
   publicPath: process.env.NODE_ENV === 'production'
     ? '/tree-app/'
     : '/',
+  outputDir: process.env.NODE_ENV === 'production'
+    ? `${__dirname}/lib`
+    : `${__dirname}/demo`,
   css: {
     extract: false,
   },
